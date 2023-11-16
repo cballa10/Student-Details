9283005ba6c950b7171f9f37c6eefb5d768bca88
cballa10 <cathieballa@gmail.com>
2023-11-16 16:27:51 +0300

    First commit

---
+++ StudentDetails.java
@@ -0,0 +1,13 @@
+public class StudentDetails{
+public static void main(String [] args){
+
+System.out.println("Name: Estella Brown");
+System.out.println("Age: 25");
+System.out.println("Gender: Female");
+System.out.println("Date Of Birth: 10/10/1998");
+System.out.println("RegNo: 20/04335");
+System.out.println("Course: Bachelor of Forensic Science ");
+ 
+}
+
+}
